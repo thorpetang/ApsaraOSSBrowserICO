8caf43e008ce5b9d6436741f1a650689637c13c0
thorpe <98566252+thorpetang@users.noreply.github.com>
2022-02-12 16:00:11 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  title: "Apsara OSS Browser",
+
+  appId: "Apsara OSS Browser",
+
+  appName: {
+    "en-US": "Apsara OSS Browser",
+  },
+
+  logo_png: path.join(__dirname, "./icon.png"),
+
+  logo_ico: path.join(__dirname, "./icon.icns"),
+
+  logo_ico: path.join(__dirname, "./icon.ico"),
+
+};
